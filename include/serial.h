35280846d94ffa9da548fb19826ac41040fa5cd0
js3g20 <js3g20@soton.ac.uk>
2022-03-27 22:12:14 +0100

    Implement Serial TX/RX

---
+++ include/serial.h
@@ -0,0 +1,27 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "protocol.h"
+
+#define SERIAL_BAUD 9600
+
+//tx variables
+volatile uint8_t txBuffer[MSG_BUF_LEN];
+volatile uint8_t txBufferIndex;
+uint8_t txBufferLength;
+volatile uint8_t transmitFlag;
+
+//rx variables
+uint8_t rxBuffer[MSG_BUF_LEN];
+volatile uint8_t rxBufferIndex;
+volatile uint8_t rxBufferReceived;
+
+void serial_init(void);
+void serial_reset(void);
+void serial_tx(uint8_t bufferSize);
+
+
+#endif
---
+++ src/serial.c
@@ -0,0 +1,125 @@
+#include "../include/serial.h"
+#include "../include/io.h"
+
+
+ISR(UART_RX_vect) {
+
+    //check for buffer overflow (invalid message)
+    if(rxBufferIndex >= (MSG_BUF_LEN - 1)) {
+
+        rxBufferReceived = 0;
+        rxBufferIndex = 0;
+        rxBuffer[0] = 0;
+    }
+
+    //read in value to buffer
+    rxBuffer[rxBufferIndex] = UDR0;
+
+    //handle start character
+    if(rxBuffer[rxBufferIndex] == RX_START_CHAR) {
+
+        //rxBuffer[0] = rxBuffer[rxBufferIndex];
+        rxBufferIndex = 1;
+    }
+
+    //handle end character
+    else if(rxBuffer[rxBufferIndex] == END_CHAR) {
+        
+        rxBufferReceived = 1;
+        rxBufferIndex = 0;
+
+        set_rx_flow();
+    }
+
+    //increment buffer index
+    else if(rxBufferIndex > 0) {
+
+        rxBufferIndex++;
+    }
+}
+
+
+ISR(UART_TX_vect) {
+
+    //if data still to send
+    if(txBufferIndex < txBufferLength) {
+
+        UDR0 = txBuffer[txBufferIndex];
+        txBufferIndex++;
+    }
+
+    //send end character
+    else {
+
+        UDR0 = '\n';
+
+        //disable tx interrupt
+        UCSR0B &= ~(_BV(TXCIE0));
+
+        transmitFlag = 0;
+    }
+}
+
+
+void serial_init(void) {
+
+    //set baud rate
+    UBRR0H = (F_CPU/(SERIAL_BAUD*16L)-1) >> 8;
+	UBRR0L = (F_CPU/(SERIAL_BAUD*16L)-1);
+
+    //enable tx and rx
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+
+    //enable rx interrupt
+    UCSR0B |= _BV(RXCIE0);
+
+    //enable tx interrupt
+    UCSR0B |= _BV(TXCIE0);
+
+    //set to data length to 8 bits
+    UCSR0C = _BV(UCSZ00) | _BV(UCSZ01);
+
+    serial_reset();
+}
+
+void serial_reset(void) {
+
+    //disable tx interrupt
+    UCSR0B &= ~(_BV(TXCIE0));
+
+    //reset tx variables
+    txBufferIndex = 0;
+    txBufferLength = 0;
+    transmitFlag = 0;
+
+    //reset rx variables
+    rxBufferIndex = 0;
+    rxBufferReceived = 0;
+
+    //set flow control
+    set_rx_flow();
+}
+
+
+void serial_tx(uint8_t bufferSize) {
+
+    //wait for write complete
+    while(!(UCSR0A & _BV(UDRE0)));
+
+    
+    if(bufferSize < MSG_BUF_LEN) {
+        
+        //set flow control
+        set_tx_flow();
+
+        txBufferLength = bufferSize;
+        transmitFlag = 1;
+        txBufferIndex = 1;
+
+        //send first value
+        UDR0 = txBuffer[0];
+
+        //enable tx interrupt
+        UCSR0B |= _BV(TXCIE0);
+    }
+}
