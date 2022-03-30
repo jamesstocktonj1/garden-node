81ed0d5a53676053a133d0159fff942be710c006
js3g20 <js3g20@soton.ac.uk>
2022-03-30 23:34:10 +0100

    Update main.c
    Add parsing for the data buffer to interpret incoming data.

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 
 #include "include/io.h"
 #include "include/temperature.h"
 #include "include/protocol.h"
 #include "include/serial.h"
 
+#define MY_NODE '1'
+
 void set_buffer(void);
+void parse_data(void);
+
 
 
 int main() {
@@ -16,20 +21,18 @@ int main() {
     init_temperature();
 
     serial_init();
+    serial_reset();
     sei();
     
-    set_buffer();
+    
     
 
     while(1) {
-        
-        serial_tx(10);
 
-        set_led1();
-        _delay_ms(800);
+        if(rxBufferReceived) {
 
-        clear_led1();
-        _delay_ms(200);
+            parse_data();
+        }
     }
 
     return 0;
@@ -49,4 +52,73 @@ void set_buffer() {
     txBuffer[8] = 'e';
     txBuffer[9] = '!';
     txBuffer[10] = '\n';
+}
+
+
+
+void parse_data() {
+
+    char localBuffer[MSG_BUF_LEN];
+
+    //create local copy (frees up for incomming messages)
+    //memcpy(localBuffer, rxBuffer, MSG_BUF_LEN);
+
+    int i;
+    for(i=0; i<MSG_BUF_LEN; i++) {
+        localBuffer[i] = rxBuffer[i];
+    }
+
+    //reset buffer flag
+    rxBufferReceived = 0;
+
+    //handle data sent to node
+    if(localBuffer[PROT_POS_NODEID] == MY_NODE) {
+        //set_led1();
+        switch(localBuffer[PROT_POS_ITEM]) {
+
+            case PROT_ITEM_LED1:
+                if(localBuffer[PROT_POS_DATA1] == '1') {
+                    set_led1();
+                }
+                else {
+                    clear_led1();
+                }
+                break;
+
+            case PROT_ITEM_LED2:
+                if(localBuffer[PROT_POS_DATA1] == '1') {
+                    set_led2();
+                }
+                else {
+                    clear_led2();
+                }
+                break;
+
+            case PROT_ITEM_RELAY:
+                if(localBuffer[PROT_POS_DATA1] == '1') {
+                    set_relay();
+                }
+                else {
+                    clear_relay();
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        //simple response
+        txBuffer[0] = REPLY_START_CHAR;
+        txBuffer[1] = 'A';
+        txBuffer[2] = END_CHAR;
+
+        //serial_tx(3);
+    }
+
+    //handle global broadcast
+    else if(localBuffer[PROT_POS_NODEID] == BDCAST_CHAR) {
+        //handle broadcast data
+    }
+
+    //clear_led1();
 }
