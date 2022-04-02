da02ce16b1766519e6ec0db74c44bb2b973117c6
js3g20 <js3g20@soton.ac.uk>
2022-04-02 21:39:31 +0100

    Update timer.h

--- include/timer.h
+++ include/timer.h
@@ -3,9 +3,12 @@
 
 #include <avr/io.h>
 
+//#define msTIMER_RELOAD_VALUE 131
+#define msTIMER_RELOAD_VALUE (256-( (F_CPU/64) / 1000) )
+
 volatile extern uint16_t msTimer;
 volatile extern uint16_t msCommsTimeout;
 
-void ConfigT0_ISR(void);
+void init_timer(void);
 
 #endif// __TIMER_H
