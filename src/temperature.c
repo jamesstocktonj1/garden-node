a86e34c2917d8dd36b33f4f0f6e83e23830eff45
js3g20 <js3g20@soton.ac.uk>
2022-04-04 22:23:59 +0100

    Update temperature
    Add averaging buffer to temperature.

--- src/temperature.c
+++ src/temperature.c
@@ -1,5 +1,8 @@
-#include <avr/io.h>
 #include "../include/temperature.h"
+
+
+uint8_t buffer_pointer;
+uint16_t temperature_buffer[BUFFER_SIZE];
 
 
 void init_temperature() {
@@ -14,7 +17,9 @@ void init_temperature() {
 
     //enable ADC
     ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
- 
+
+    //initialise buffer
+    buffer_pointer = 0;
 }
 
 
@@ -34,4 +39,25 @@ uint16_t get_temperature() {
 
     //return (uint16_t)(((uint8_t)ADCH << 2) + ((uint8_t)ADCL >> 6));
     return adc;
+}
+
+void buffer_temperature() {
+
+    //read temperature
+    temperature_buffer[buffer_pointer] = get_temperature();
+    
+    //increment buffer
+    buffer_pointer = (buffer_pointer < (BUFFER_SIZE - 2)) ? buffer_pointer + 1 : 0;
+}
+
+uint16_t get_buffer_temperature() {
+
+    uint32_t total = 0;
+    int i;
+
+    for(i=0; i<BUFFER_SIZE; i++) {
+        total += temperature_buffer[i];
+    }
+
+    return total / BUFFER_SIZE;
 }
